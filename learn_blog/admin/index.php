03a1adb0e16dd8a90b58380e48613992d8ecdb01
iamdev18 <iamdev@ua.fm>
2015-10-13 00:26:47 +0300

    Додані функції, але поки що вони пусті .

--- learn_blog/admin/index.php
+++ learn_blog/admin/index.php
@@ -2,6 +2,21 @@
 	
 
  	// тут буде скоро функції адмін-панелі
+	function add_article(){
+
+	}
+
+	function view_article(){
+
+	}
+	
+	function edit_article(){
+
+	}
+
+	function delete_article(){
+
+	}
 
 
 ?>
